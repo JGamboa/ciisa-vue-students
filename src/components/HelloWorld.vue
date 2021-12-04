6a01b230927590b75a99faec3e8801b2c30b69b5
Joaquín Gamboa Figueroa <joaquin.gamboa@dsmgroup.cl>
2021-12-04 11:04:39 -0300

    eliminar

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -47,6 +47,14 @@
                                         <path d="M20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.2-.2-.45-.29-.71-.29s-.51.1-.7.29l-1.83 1.83 3.75 3.75 1.83-1.83zM3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM5.92 19H5v-.92l9.06-9.06.92.92L5.92 19z"></path>
                                     </svg>
                                   </button>
+
+                                  <button v-on:click="destroy(course.id)" type="button" class="flex items-center px-5 py-2.5 font-medium tracking-wide text-black capitalize rounded-md  hover:bg-red-200 hover:fill-current hover:text-red-600  focus:outline-none  transition duration-300 transform active:scale-95 ease-in-out">
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px">
+                                        <path d="M0 0h24v24H0V0z" fill="none"></path>
+                                        <path d="M8 9h8v10H8z" opacity=".3"></path>
+                                        <path d="M15.5 4l-1-1h-5l-1 1H5v2h14V4zM6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM8 9h8v10H8V9z"></path>
+                                    </svg>
+                                  </button>
                                 </td>
                             </tr>
                         </tbody>
@@ -93,16 +101,7 @@
                      <button type="button" v-on:click="create"><span class="pl-2 mx-1">Save</span></button>
                   </button>
                </div>
-               <div class="flex-initial">
-                  <button type="button" class="flex items-center px-5 py-2.5 font-medium tracking-wide text-black capitalize rounded-md  hover:bg-red-200 hover:fill-current hover:text-red-600  focus:outline-none  transition duration-300 transform active:scale-95 ease-in-out">
-                     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px">
-                        <path d="M0 0h24v24H0V0z" fill="none"></path>
-                        <path d="M8 9h8v10H8z" opacity=".3"></path>
-                        <path d="M15.5 4l-1-1h-5l-1 1H5v2h14V4zM6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM8 9h8v10H8V9z"></path>
-                     </svg>
-                     <span class="pl-2 mx-1">Delete</span>
-                  </button>
-               </div>
+              
             </div>
          </div>
         
@@ -148,6 +147,18 @@ export default {
           teacher_name: null,
           name: null,
         };
+    },
+    destroy(id){
+      this.errors = null;
+      this.$http.delete('http://students.test/api/courses/' + id)
+      .then(response => {
+        alert(response.data.message);
+        this.get();
+      })
+      .catch(e => {
+        this.errors= e.response.data.errors;
+        alert(e.response.data);
+      })
     },
     create(){
       this.errors = null;
